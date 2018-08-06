8823cd7edd5304a3d6980e0feea4047dd2c9ffc7
wentao.liu <12345678>
2018-08-06 16:17:38 +0800

    Initial commit

---
+++ src/com/allen/activeMQ/testCase/JMSConsumer.java
@@ -0,0 +1,87 @@
+package com.allen.activeMQ.testCase;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/**
+ * Project: CCC Web Suite
+ * <p>
+ * Created by Administrator on 2018/8/6.
+ * <p>
+ * Copyright 2017 CCC Corporation Limited.
+ * All rights reserved.
+ * <p>
+ * This software is the confidential and proprietary information of
+ * CCC Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with http://ccc.cccis.com/
+ */
+/**
+ * 消息的消费者（接受者）
+ * @author Allen
+ *
+ */
+public class JMSConsumer {
+    private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;//默认连接用户名
+    private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;//默认连接密码
+    private static final String BROKEURL = ActiveMQConnection.DEFAULT_BROKER_URL;//默认连接地址
+
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory;//连接工厂
+        Connection connection = null;//连接
+
+        Session session;//会话 接受或者发送消息的线程
+        Destination destination;//消息的目的地
+
+        MessageConsumer messageConsumer;//消息的消费者
+
+        //实例化连接工厂
+        connectionFactory = new ActiveMQConnectionFactory(JMSConsumer.USERNAME, JMSConsumer.PASSWORD, JMSConsumer.BROKEURL);
+
+        try {
+            //通过连接工厂获取连接
+            connection = connectionFactory.createConnection();
+            //启动连接
+            connection.start();
+            //创建session
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            //创建一个连接HelloWorld的消息队列
+            destination = session.createQueue("HelloWorld");
+            //创建消息消费者
+            messageConsumer = session.createConsumer(destination);
+
+            while (true) {
+                TextMessage textMessage = (TextMessage) messageConsumer.receive(1000*60);
+                if(textMessage != null){
+                    System.out.println("收到 Allen 的消息:" + textMessage.getText());
+                }else {
+                    System.out.println("太久没收到 Allen 的消息了，溜了溜了");
+                    break;
+                }
+            }
+
+            session.commit();
+
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
+            if(connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
